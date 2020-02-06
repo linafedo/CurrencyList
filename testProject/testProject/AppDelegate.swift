bfb7c200df948f5828293483b5f1cf52aeb3671f
linafedo <gv.fedo@gmail.com>
2020-02-06 15:05:45 +0300

    create project

---
+++ testProject/testProject/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  testProject
+//
+//  Created by Galina Fedorova on 05.02.2020.
+//  Copyright © 2020 Galina Fedorova. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
