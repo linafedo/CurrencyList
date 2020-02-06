ab4e14096a40aa8fde9e0eb722d322e855aa5345
linafedo <gv.fedo@gmail.com>
2020-02-06 15:44:43 +0300

    remove storyboard

--- testProject/testProject/AppDelegate.swift
+++ testProject/testProject/AppDelegate.swift
@@ -11,8 +11,20 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window: UIWindow = {
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            window.backgroundColor = .systemBackground
+            window.rootViewController = RatesViewController()
+            window.makeKeyAndVisible()
+            return window
+        }()
+        
+        self.window = window
+
         return true
     }
 
---
+++ testProject/testProject/View/RatesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RatesViewController.swift
+//  testProject
+//
+//  Created by Galina Fedorova on 06.02.2020.
+//  Copyright © 2020 Galina Fedorova. All rights reserved.
+//
+
+import UIKit
+
+class RatesViewController: UIViewController {
+
+//    var tableView: UITableView!
+
+}
+
+// MARK: - Life сycle methods
+
+extension RatesViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setup()
+    }
+    
+}
+
+// MARK: - Setup
+
+extension RatesViewController {
+    
+    func setup() {
+        self.view.backgroundColor = .red
+        
+    }
+
+}
